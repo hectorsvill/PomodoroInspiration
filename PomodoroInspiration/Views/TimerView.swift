0240b374861f8465d573088b8ebb2956d83fc96d
hectorsvill <hectorstevenvillasano@gmail.com>
2020-10-15 15:50:00 -0700

    strokeStyleLineWidth

--- PomodoroInspiration/Views/TimerView.swift
+++ PomodoroInspiration/Views/TimerView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct TimerView: View {
+    let strokeStyleLineWidth: CGFloat = 10
     @Binding var timerType: TimerType
     @Binding var circleTimer: CGFloat
     @Binding var timerTitle: String
@@ -18,17 +19,16 @@ struct TimerView: View {
                 .trim(from: 0, to: 1)
                 .stroke(
                     Color.gray.opacity(0.10),
-                    style: StrokeStyle(lineWidth: 5, lineCap: .round)
+                    style: StrokeStyle(lineWidth: strokeStyleLineWidth, lineCap: .round)
                 )
-                .frame(width: 400, height: 400)
+                
             
             Circle()
                 .trim(from: 0, to: circleTimer)
                 .stroke(
                     timerType == .breakTimer ? Color.blue : .red,
-                    style: StrokeStyle(lineWidth: 5, lineCap: .round)
+                    style: StrokeStyle(lineWidth: strokeStyleLineWidth, lineCap: .round)
                 )
-                .frame(width: 400, height: 400)
                 .rotationEffect(.init(degrees: -90))
                 .animation(.easeOut)
             
