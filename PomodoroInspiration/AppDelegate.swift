cb936eeb8bab108ca22fe94874b69a6564474e21
hectorsvill <hectorstevenvillasano@gmail.com>
2020-10-20 14:21:06 -0700

    refactor styleMask, add app category

--- PomodoroInspiration/AppDelegate.swift
+++ PomodoroInspiration/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Create the window and set the content view.
         window = NSWindow(
             contentRect: NSRect(x: 0, y: 0, width: 500, height: 500),
-            styleMask: [.titled, .closable, .miniaturizable, .fullSizeContentView],
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
             backing: .buffered, defer: false)
         window.isReleasedWhenClosed = false
         window.center()
